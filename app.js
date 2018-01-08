6d248f16dfb196e2d7349617d8097a5cc53cf90f
Brian Bixby <brianbixby0@gmail.com>
2018-01-08 10:43:15 -0800

    fixed typo

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function formData(event) {
 function createTable() {
   var row;
   for (var i = 0; i < data.length; i++) {
-    row = document.creatElement('tr');
+    row = document.createElement('tr');
     row.innerHTML = '<td>' + data[i].first + '</td>' +
       '<td>' + data[i].last + '</td>' +
       '<td>' + data[i].enrolled + '</td>' +
